cfb9d619bdac34099461f9226876131093b45528
Peter Ochan <rempeterwisdom@gmail.com>
2023-11-30 23:31:02 +0300

    repo set up and pascal solution

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" 
+    Module that prints list of lists of
+    integers representing Pascals triangle
+"""
+
+from typing import List
+
+
+def pascal_triangle(n: int) -> List[list]:
+    '''
+    function that implements Pascal's triangle
+    '''
+    if n <= 0:
+        return []
+
+    if n == 1:
+        return [[1]]
+
+    if n == 2:
+        return [[1], [1, 1]]
+
+    triangle = [[1], [1, 1]]
+
+    for i in range(2, n):
+        temp = [1, 1]
+        for j in range(0, len(triangle[-1])-1):
+            a = triangle[-1][j]
+            b = triangle[-1][j+1]
+            temp.insert(-1, a + b)
+        triangle.append(temp)
+
+    return triangle
---
+++ README.md
@@ -0,0 +1,1 @@
+This includes common interview questions
