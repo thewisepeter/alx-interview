11a22adb7921639a9ac154fff4d006455316ba89
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-05 16:54:01 +0300

    added documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+'''
+    A module with a solution to lock boxes interview
+    question
+'''
 
 
 def canUnlockAll(boxes):
+    ''' Function that implements solution to lockboxes '''
     unlocked = [0]
     for box_id, box in enumerate(boxes):
         if not box:
