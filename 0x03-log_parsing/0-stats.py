de057e116e6ad0d456397037ea2151d37595465c
Peter Ochan <rempeterwisdom@gmail.com>
2023-12-21 13:16:50 +0300

    Updated simpler solution to log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,61 +1,65 @@
 #!/usr/bin/python3
 '''
-     script that reads stdin line by line and computes metrics
+    script that reads stdin line by line and computes metrics:
+
+    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+    <status code> <file size> (if the format is not this one, the line
+    must be skipped)
+    After every 10 lines and/or a keyboard interruption (CTRL + C)
+    print these statistics from the beginning:
+    Total file size: File size: <total size>
+    where <total size> is the sum of all previous <file size>
+    (see input format above)
+    Number of lines by status code:
+    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+    if a status code doesn’t appear or is not an integer, don’t print
+    anything for this status code
+    format: <status code>: <number>
+    status codes should be printed in ascending order
 '''
 
-import re
+
 import sys
 
+# Posiible status codes
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                '404': 0, '405': 0, '500': 0}
 
-def process_line(line):
-    """
-    Process a line and extract information.
-    """
-    # Define the regex pattern for the input format
-    pattern = re.compile(r'(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)')
+total_size = 0
+line_count = 0
 
-    match = pattern.match(line)
-    if match:
-        ip_address, date, status_code, file_size = match.groups()
-        return int(status_code), int(file_size)
-    else:
-        return None
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
 
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-def print_statistics(total_size, status_counts):
-    """
-    Print the computed statistics.
-    """
-    print(f"Total file size: {total_size}")
-    for status_code in sorted(status_counts):
-        print(f"{status_code}: {status_counts[status_code]}")
+            # check validity of status code and increase count
+            if status_code in status_codes.keys():
+                status_codes[status_code] += 1
 
+            # update total size
+            total_size += file_size
 
-def main():
-    total_size = 0
-    status_counts = {}
+            # update count of lines
+            line_count += 1
 
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            data = process_line(line.strip())
-            if data:
-                status_code, file_size = data
-                total_size += file_size
+        if line_count == 10:
+            line_count = 0  # reset count for next round
+            print('File size: {}'.format(total_size))
 
-                if status_code in status_counts:
-                    status_counts[status_code] += 1
-                else:
-                    status_counts[status_code] = 1
+            # print out status code counts
+            for key, value in sorted(status_codes.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-            # Print statistics after every 10 lines
-            if i % 10 == 0:
-                print_statistics(total_size, status_counts)
-                print("-----")
+except Exception as err:
+    pass
 
-    except KeyboardInterrupt:
-        # Handle keyboard interruption (CTRL + C)
-        print_statistics(total_size, status_counts)
-
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
