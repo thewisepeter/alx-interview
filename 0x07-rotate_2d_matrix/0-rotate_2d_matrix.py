7095cda9fec5b5a5050f37bb621e32eb137b62c0
thewisepeter <rempeterwisdom@gmail.com>
2024-01-25 14:41:15 +0300

    fixed pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,9 +5,9 @@
 
 
 '''def transpose_in_place(matrix: list) -> None:
-    '''
+
         Transposes a matrix in-place
-    '''
+
     rows, cols = len(matrix), len(matrix[0])
 
     for i in range(rows):
@@ -17,9 +17,9 @@
 
 
 def reverse_matrix(matrix: list) -> None:
-    '''
+
         reverses a list
-    '''
+
 
     rows, cols = len(matrix), len(matrix[0])
 
@@ -32,9 +32,9 @@ def reverse_matrix(matrix: list) -> None:
 
 
 def rotate_2d_matrix(matrix: list) -> None:
-    '''
+
         rotates a 2d matrix
-    '''
+
     transpose_in_place(matrix)
     reverse_matrix(matrix)
 '''
