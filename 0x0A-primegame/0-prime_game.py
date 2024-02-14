1404dccf22c3cd7dc1501f7e5933040709a6f9ac
thewisepeter <rempeterwisdom@gmail.com>
2024-02-14 21:09:44 +0300

    repo set up and solution

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -105,27 +105,27 @@
 
 def isWinner(x, nums):
     '''
-        function that checks for the winner
+        determines the winner
     '''
     if not nums or x < 1:
         return None
     max_num = max(nums)
 
-    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    filter = [True for _ in range(max(max_num + 1, 2))]
     for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not my_filter[i]:
+        if not filter[i]:
             continue
         for j in range(i * i, max_num + 1, i):
-            my_filter[j] = False
-    my_filter[0] = my_filter[1] = False
+            filter[j] = False
+    filter[0] = filter[1] = False
     y = 0
-    for i in range(len(my_filter)):
-        if my_filter[i]:
+    for i in range(len(filter)):
+        if filter[i]:
             y += 1
-        my_filter[i] = y
+        filter[i] = y
     player1 = 0
     for x in nums:
-        player1 += my_filter[x] % 2 == 1
+        player1 += filter[x] % 2 == 1
     if player1 * 2 == len(nums):
         return None
     if player1 * 2 > len(nums):
