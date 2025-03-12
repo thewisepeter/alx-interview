91361a8259398eb92131a92345b8101ba5f19da6
Peter Wisdom <rempeterwisdom@gmail.com>
2025-03-12 19:53:52 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,10 @@
 """
     Script that prints list of lists of integers
     representing the Pascal’s triangle of n
+    Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:
+
+    Returns an empty list if n <= 0
+    You can assume n will be always an integer
 """
 
 
